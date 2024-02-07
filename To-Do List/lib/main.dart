4d524c662c4b7a82d91067bffee9752fc567a7a7
jayasurya <jayasuryakarthi55@gmail.com>
2024-02-08 01:49:49 +0530

    Commit

--- To-Do List/lib/main.dart
+++ To-Do List/lib/main.dart
@@ -11,38 +11,124 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.blue,
           accentColor: Colors.amber,
+        ).copyWith(
+          secondary: Colors.deepOrange,
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Todo List'),
+      home: const LoginPage(),
     );
   }
 }
 
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _mailController = TextEditingController();
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MyHomePage(
+            title: 'Todo List',
+            name: _nameController.text,
+            mailId: _mailController.text,
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter a name...',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _mailController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter a mail id...',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a mail id';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _login,
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title, required this.name, required this.mailId})
+      : super(key: key);
 
   final String title;
+  final String name;
+  final String mailId;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> todos = [];
+  List<TodoItem> todos = [];
   TextEditingController _todoController = TextEditingController();
 
   void _addTodo() {
     if (_todoController.text.isNotEmpty) {
       setState(() {
-        todos.add(_todoController.text);
+        todos.add(TodoItem(
+          title: _todoController.text,
+          isDone: false,
+          timestamp: DateTime.now(),
+        ));
         _todoController.clear();
       });
     }
@@ -54,6 +140,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _toggleDone(int index) {
+    setState(() {
+      todos[index] = todos[index].copyWith(isDone: !todos[index].isDone);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +153,34 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello, ${widget.name}!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.yellow),
+                    Icon(Icons.star, color: Colors.yellow),
+                    Icon(Icons.star, color: Colors.yellow),
+                  ],
+                ),
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
@@ -79,10 +198,36 @@ class _MyHomePageState extends State<MyHomePage> {
                 return Card(
                   elevation: 3,
                   margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  color: todos[index].isDone ? Colors.green.shade100 : null,
                   child: ListTile(
-                    title: Text(
-                      todos[index],
-                      style: TextStyle(fontSize: 18),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: todos[index].isDone,
+                              onChanged: (_) => _toggleDone(index),
+                            ),
+                            Text(
+                              todos[index].title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                decoration: todos[index].isDone
+                                    ? TextDecoration.lineThrough
+                                    : null,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'Time: ${_formatTime(todos[index].timestamp)}',
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
                     trailing: IconButton(
                       icon: Icon(Icons.delete),
@@ -100,6 +245,34 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Add Todo',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  String _formatTime(DateTime time) {
+    return '${time.hour}:${time.minute}';
+  }
+}
+
+class TodoItem {
+  final String title;
+  final bool isDone;
+  final DateTime timestamp;
+
+  TodoItem({
+    required this.title,
+    required this.isDone,
+    required this.timestamp,
+  });
+
+  TodoItem copyWith({
+    String? title,
+    bool? isDone,
+    DateTime? timestamp,
+  }) {
+    return TodoItem(
+      title: title ?? this.title,
+      isDone: isDone ?? this.isDone,
+      timestamp: timestamp ?? this.timestamp,
     );
   }
 }
